26124eb33c256be99cf3ccb661bd54929e3e12ba
Powercs12 <1968103220@qq.com>
2022-11-30 22:25:07 +0800

    第一次提交

---
+++ spring-cloud/cloud-demo/eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 崔帅
+ * @version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+
+}
